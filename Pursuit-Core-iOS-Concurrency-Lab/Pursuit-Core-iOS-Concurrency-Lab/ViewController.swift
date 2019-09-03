4f738dcc42ff960936b1e9492af216d519e468bb
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-09-03 15:00:38 -0400

    Initial commit. Creates XcodeProj. Sets plist to allow parsing data from non secure source.

---
+++ Pursuit-Core-iOS-Concurrency-Lab/Pursuit-Core-iOS-Concurrency-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pursuit-Core-iOS-Concurrency-Lab
+//
+//  Created by Mariel Hoepelman on 9/3/19.
+//  Copyright © 2019 Mariel Hoepelman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
